38ea74ce5a7a3b0b8799fd5f3cccf795e21046a7
J Apacible <japacible@gmail.com>
2013-04-20 08:23:35 -0700

    file upload things

---
+++ assets/js/generateRunway.js
@@ -0,0 +1,203 @@
+var selected = {"top" : [], 
+				"bottom" : [],
+				"shoe" : []
+			   };
+
+var weather = "";
+
+$(document).ready(function() {
+	$(".main-content-2").hide();
+	$.get("http://pingyang.me/fashionation/api/gettable.php?table=tops", populateShirts);
+	$.get("http://pingyang.me/fashionation/api/gettable.php?table=bottoms", populatePants);
+	$.get("http://pingyang.me/fashionation/api/gettable.php?table=shoes", populateShoes);
+	$("#generate").click(generateOutfit);
+	$("#random").click(randomOutfit);
+	getWeather();
+	setTimeout(populateWeatherStats, 700);
+	setTimeout(populateWeatherStats, 1400);
+});
+
+function populateWeatherStats() {
+	getWeather();
+
+	var code = getWeatherCode();
+	var temp = getTemperature();
+	var stat = getWeatherStatus();
+	var loc = getCity();
+	console.log("code");
+	console.log(code);
+	console.log("temp");
+	console.log(temp);
+	console.log("stat");
+	console.log(stat);
+	console.log('loc');
+	console.log(loc);
+	$("#city").text(loc);
+	$("#stat").text(stat);
+	$("#temp").text(temp);
+}
+
+function populateShirts(shirtObj) {
+	shirtObj = $.parseJSON(shirtObj);
+	populateLoop("top", shirtObj);
+}
+
+function populateShoes(shoeObj) {
+	shoeObj = $.parseJSON(shoeObj);
+	populateLoop("shoe", shoeObj);
+}
+
+function populatePants(pantsObj) {
+	pantsObj = $.parseJSON(pantsObj);
+	populateLoop("bottom", pantsObj);
+}
+
+function populateLoop(string, array) {
+	for(var i = 0; i < array.length; i++) {
+		var id = array[i]["id"];
+		var name = array[i]["name"];
+
+		var element = $("<div>");
+		element.addClass(string);
+		element.addClass("clothing");
+		element.attr("id", id);
+		element.attr("alt", name)
+		element.click(highlight);
+		element.mouseenter(hoverPreview);
+
+		var img = $("<img>");
+    if (string == "shoe")
+      img.attr("src", "assets/img/aoc_icons/shoes.png");
+    else
+      img.attr("src", "assets/img/aoc_icons/" + id + ".png");
+		img.attr("alt", name);
+
+		element.append(img);
+		var parent = "#" + string + "s";
+		$(parent).append(element);
+	}
+}
+
+function highlight() {
+	var item = $(this);
+	var type = item.attr("class").split(" ")[0];
+	var name = item.attr("id");
+
+	if(item.hasClass("highlight")) {
+		// remove highlighting, remove from array
+		item.removeClass("highlight");
+		selected[type] = jQuery.grep(selected[type], function(value) {
+				return value != name;
+		});
+	} else {
+		// add highlighting, check for max items, add to array
+		if((type != "top" && selected[type].length == 0) || (type == "top" && selected[type].length < 2)) {
+			item.addClass("highlight");
+			selected[type].push(name);
+		} else {
+			//ERROR!
+			if(type == "top") {
+				// more than two shirt error
+				$("#tops-error").css("visibility", "visible");
+			setTimeout(function() {
+				$("#tops-error").css("visibility", "hidden");
+			}, 800);
+			} else if (type == "bottom") {
+				$("#bottoms-error").css("visibility", "visible");
+			setTimeout(function() {
+				$("#bottoms-error").css("visibility", "hidden");
+			}, 800);
+			} else if (type = "shoe") {
+				$("#shoes-error").css("visibility", "visible");
+			setTimeout(function() {
+				$("#shoes-error").css("visibility", "hidden");
+			}, 800);
+			}
+		}
+	}
+}
+
+function generateOutfit() {
+	var top1_id = "";
+	var top2_id = "";
+	var bottom_id = "";
+	var shoe_id = "";
+
+	if(selected["top"][0]) {
+		top1_id = selected["top"][0];
+	}
+	if(selected["top"][1]) {
+		top2_id = selected["top"][1];
+	}
+	if(selected["bottom"][0]) {
+		bottom_id = selected["bottom"][0];
+	}
+	if(selected["shoe"][0]) {
+		shoe_id = selected["shoe"][0];
+	}
+
+	$.get("http://pingyang.me/fashionation/api/getoutfits.php?top1=" + top1_id + "&top2=" + top2_id + 
+			"&bottom=" + bottom_id + "&shoe=" + shoe_id + "&weather=" + weather, displayOutfit);
+}
+
+function randomOutfit() {
+	$.get("http://pingyang.me/fashionation/api/getoutfits.php?top1=&top2=&bottom=&shoe=&weather=" + weather, displayOutfit);
+}
+
+function displayOutfit(generatedOutfit) {
+	generatedOutfit =  $.parseJSON(generatedOutfit);
+	var index = Math.floor(Math.random() * generatedOutfit.length);
+	generatedOutfit = generatedOutfit[index];
+
+	$(".main-content").hide();
+	$(".extra-content").hide();
+	$(".main-content-2").show();
+
+	// fill in images with selections
+	var holdAllTheImages = $("#generated-outfit-aoc");
+	var concat =(String) ("" + generatedOutfit["top1"] + "" +  generatedOutfit["top2"] + "" + generatedOutfit["bottom"] + "" + generatedOutfit["shoe"]);
+	var image = $("<img>");
+	image.attr("src", concat + ".jpg");
+	$("#generated-outfit-image").prepend(image);
+
+	holdAllTheImages = addToMainDiv(holdAllTheImages, "top1", generatedOutfit);
+	if(generatedOutfit["top2"]) {
+		holdAllTheImages = addToMainDiv(holdAllTheImages, "top2", generatedOutfit);
+	} 
+	holdAllTheImages = addToMainDiv(holdAllTheImages, "bottom", generatedOutfit);
+	holdAllTheImages = addToMainDiv(holdAllTheImages, "shoe", generatedOutfit);
+}
+
+function addToMainDiv(mainDiv, string, array) {
+	var img = $("<img>");
+	var div = $("<div>");
+	div.addClass("clothing");
+	div.addClass("large");
+	img.attr("src", array[string]);
+	div.append(img);
+	mainDiv.append(div);
+	return mainDiv;
+}
+
+function hoverPreview() {
+	var id = $(this).attr("id");
+	var full = $(this).attr("alt");
+	var paragraph = $("#aoc-help");
+	var image = $("#aoc-help-image");
+	paragraph.html("This is a preview of what <strong>" + full + "</strong> might look like.");
+	image.attr("src", "assets/img/aoc_image/" + id + ".jpg");
+}
+
+function grabUploadedImages() {
+	$.get("http://pingyang.me/fashionation/api/getPhotos.php", displayPulledImages);
+}
+
+function displayPulledImages(array) {
+	var ul = $("<ul>");
+	for(var i = 0; i < array.length; i++) {
+		var img = $("<img>");
+		img.attr("src", array[i]);
+		ul.append(img);
+	}
+	$(body).append(ul);
+}
--- result.php
+++ result.php
@@ -1,12 +1,83 @@
-<?php $name = $_FILES["image"]["name"];
-$file_type = $_FILES["image"]["type"];
-$file_size = $_FILES["image"]["size"];
-if($file_size <= 512000 && ($file_type == "image/jpeg" ||
-							$file_type == "image/jpg" || 
-							$file_type == "image/png")) {
-move_uploaded_file($_FILES["image"]["tmp_name"], "uploadedimages/$name");
-print("saved! at /uploadedimages/$name"); 
-} else {
-	print("bad file type or size");
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>fashionation</title>
+    <link href="assets/css/reset.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/folksfont.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/style.css" type="text/css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  </head>
+
+  <body>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="http://students.washington.edu/apacible/geo/modernizr-2.5.2.js"></script>
+      <script src="assets/js/generateRunway.js"></script>
+      <div class="container">
+        <header>
+          <h1>fashionation</h1>
+        </header>
+        <nav>
+          <div>
+            <h2><a href="home.html" title="Home">HOME</a></h2>
+          </div>
+          <div>
+            <h2><a href="coordi.html" title="Outfit Suggestor">COORDI</a></h2>
+          </div>
+          <div class="vertical-ruler"></div>
+          <div>
+            <h2><a href="resources.html" title="Tutorials">BASICS</a></h2>
+          </div>
+        </nav>
+
+        <div class="main-container">
+          <div class="nav-shadow">
+          </div>
+          <div class="main-content">
+          	<!--File Upload-->
+            <form action="result.php" method="post" enctype="multipart/form-data">
+				  		Upload an image:
+				  		<input type="file" name="image" />
+				  		<input type="submit" />
+						</form>
+
+						<?php 
+							$name = $_FILES["image"]["name"];
+							$file_type = $_FILES["image"]["type"];
+							$file_size = $_FILES["image"]["size"];
+							if($file_size <= 512000 && ($file_type == "image/jpeg" ||
+														$file_type == "image/jpg" || 
+														$file_type == "image/png")) {
+							move_uploaded_file($_FILES["image"]["tmp_name"], "uploadedimages/$name");
+							print("saved! at /uploadedimages/$name"); 
+							} else {
+								print("bad file type or size");
+							}
+						?>
+						<!--Wardrobe-->
+            <div id="wardrobe">
+              <div class="article-of-clothing" id="tops">
+              </div>
+            </div>
+            <div id="outfit-buttons">
+              <div class="outfit-button" id="generate">
+                coordinate
+              </div>
+              <div class="outfit-button" id="random">
+                random
+              </div>
+            </div>
+          </div> <!--end main-content -->
+          
+
+          <div class="extra-content">
+          </div><!-- end extra content-->
+
+        </div> <!-- end main container -->
+      </div>
+  </body>
+</html>
+
+
+
+
+
