e3e5f2e1fef5535296eab71cebe67f8c57bd51e7
godaddy server <godaddy.server>
2013-04-20 10:40:33 -0700

    Added Runway to menu

--- get_header.php
+++ get_header.php
@@ -1,8 +1,40 @@
 <?php
-
   if(!isset($_COOKIE['usr'])){
     echo("<script>top.location.href='./'</script>");
   }
-
-
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>fashionation</title>
+    <link href="assets/css/reset.css" type:"text/css" rel="stylesheet" />
+    <link href="assets/css/folksfont.css" type:"text/css" rel="stylesheet" />
+    <link href="assets/css/style.css" type:"text/css" rel="stylesheet" />
+  </head>
+
+  <body>
+      <script src="http://code.jquery.com/jquery-latest.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <div class="container">
+        <header>
+          <h1>fashionation</h1>
+        </header>
+        <nav>
+          <div>
+            <h2><a href="home.php" title="Home">HOME</a></h2>
+          </div>
+          <div>
+            <h2><a href="coordi.php" title="Outfit Suggestor">COORDI</a></h2>
+          </div>
+          <div>
+            <h2><a href="resources.php" title="Tutorials">BASICS</a></h2>
+          </div>
+          <div>
+            <h2><a href="runway.php" title="Runway">RUNWAY</a></h2>
+          </div>
+        </nav>
+        <div class="main-container">
+          <div class="nav-shadow">
+          </div>
+
