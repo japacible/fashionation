678d29e82f25331a46a0c5e4132a2713e7200f31
Katlyn Edwards <katlyn.edwards@hotmail.com>
2013-04-20 03:47:31 -0700

    end quote D:

--- result.php
+++ result.php
@@ -1,3 +1,3 @@
 <?php $name = $_FILES["image"]["name"];
 move_uploaded_file($_FILES["image"]["tmp_name"], "uploadedimages/$name");
-print("saved! at /uploadedimages/$name); ?>
+print("saved! at /uploadedimages/$name"); ?>
