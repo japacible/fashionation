5b2fc7c12685e04d81e847130a10dec172b23664
Katlyn Edwards <katlyn.edwards@hotmail.com>
2013-04-20 11:38:49 -0700

    error hide time updated

--- assets/js/generateRunway.js
+++ assets/js/generateRunway.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	$("#generate").click(generateOutfit);
 	$("#random").click(randomOutfit);
 	if($("#errorMessageUpload")) {
-		setTimeout(hideErrorMessage, 300);
+		setTimeout(hideErrorMessage, 800);
 	}
 });
 
